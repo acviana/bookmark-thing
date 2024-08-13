4355cb4bff034d7c17fd3105d43ee465997bf791
Alex C. Viana <alexcostaviana@gmail.com>
2024-08-13 11:00:01 -0500

    Added partially functioning INSERT statement.

--- temp.sql
+++ temp.sql
@@ -1,17 +1,29 @@
---create table bookmark (
---    id uuid,
---    ctime timestamp_s,
---    mtime timestamp_s,
---    url varchar,
---    title varchar,
---    tags varchar[]
---)
-
+-- create table bookmark (
+-- id uuid,
+-- ctime timestamp_s,
+-- mtime timestamp_s,
+-- url varchar,
+-- title varchar,
+-- tags varchar[]
+-- )
 alter table bookmark
 add column is_read boolean
 ;
 
-insert 
+insert into bookmark 
+by position
+values (
+    uuid(), 
+    get_current_timestamp(), 
+    get_current_timestamp(),
+    'https://www.wired.com/story/how-30-lines-of-code-blew-up-27-ton-generator/',
+    'How 30 Lines of Code Blew Up a 27-Ton Generator', 
+    ['physical-infra'], 
+    false)
+ ;
 
-SELECT * FROM bookmark;
+select *
+from bookmark
+;
+
 describe bookmark;
