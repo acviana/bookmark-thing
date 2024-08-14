33a5826549681a8ed889b99af6423580361b5579
Alex C. Viana <alexcostaviana@gmail.com>
2024-08-13 19:53:16 -0500

    Added working table creation and insert to temp.sql

--- temp.sql
+++ temp.sql
@@ -1,13 +1,12 @@
--- create table bookmark (
--- id uuid,
--- ctime timestamp_s,
--- mtime timestamp_s,
--- url varchar,
--- title varchar,
--- tags varchar[]
--- )
-alter table bookmark
-add column is_read boolean
+create or replace table bookmark (
+    id uuid primary key,
+    ctime timestamptz,
+    mtime timestamptz,
+    url varchar,
+    title varchar,
+    is_read boolean,
+    tags varchar[]
+)
 ;
 
 insert into bookmark 
@@ -18,8 +17,9 @@ values (
     get_current_timestamp(),
     'https://www.wired.com/story/how-30-lines-of-code-blew-up-27-ton-generator/',
     'How 30 Lines of Code Blew Up a 27-Ton Generator', 
+    false,
     ['physical-infra'], 
-    false)
+ )
  ;
 
 select *
